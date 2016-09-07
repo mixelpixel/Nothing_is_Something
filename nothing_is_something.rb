7568db8ae9effc56fbdc57d2b6e248a10744c5f3
Patrick Kennedy <mixelpix@mac.com>
2016-09-06 22:03:11 -0400

    learning vim and .gitignore commands

--- nothing_is_something.rb
+++ nothing_is_something.rb
@@ -1,23 +1,31 @@
 #234567891----*----2----*----3----*----4----*----5----*----6----*-----72--*----8----*----9----*--100-----------------120
 # written by pdk with vim
+
 =begin
+
 The following are notes, thoughts and code bits from Sandi Metz's talk, "Nothing is Something"
 https://www.youtube.com/watch?v=zc9OvLzS9mU
-SENDING MESSAGES TO OBJECTS
-SmallTalk Infected
-CONDITION AVERSE
-MESSAGE CENTRIC
-ABSTRACTION SEEKING
-=end
+4 parts
+1) SMALLTALK INFECTED
+2) CONDITION AVERSE
+3) MESSAGE CENTRIC
+4) ABSTRACTION SEEKING
 
+1) SMALLTALK INFECTED
+https://en.wikipedia.org/wiki/Smalltalk
+http://stackoverflow.com/questions/1821266/what-is-so-special-about-smalltalk
+"Sending messages to objects"
+
+=end
 
 # NOTE these are synonymous:
 puts 1.to_s         # <-- "1"
 puts 1.send(:to_s)  # <-- "1"
 
-puts "Does " + 1.to_s + " == " + 1.send(:to_s) + "?  " + (1.to_s == 1.send(:to_s)).to_s 
+puts "Does " + 1.to_s + " == " + 1.send(:to_s) + "?  " + (1.to_s == 1.send(:to_s)).to_s
+puts "Ayep... 1 == 1 is " + (1 == 1).to_s
+puts "What is the true.class? " + true.class.to_s
 puts
-
 
 # NOTE these are also synonymous:
 puts "1 + 1 = " + (1 + 1).to_s                # <-- "1 + 1 = 2"
@@ -35,9 +43,11 @@ puts
 
 puts "1.class is a " + 1.class.to_s             # <-- Fixnum
 puts
-puts "Fixnum.instance_methods(false) are: \n" + Fixnum.instance_methods(false).to_s
+puts "Fixnum.instance_methods(false) are:" + Fixnum.instance_methods(false).to_s
+puts Fixnum.instance_methods(false).sort
 puts
 puts "Fixnum.instance_methods(true) are: \n" + Fixnum.instance_methods(true).to_s
+puts Fixnum.instance_methods(true).sort
 puts
 
 
