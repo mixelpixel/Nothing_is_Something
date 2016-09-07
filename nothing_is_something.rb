9e83f7eff29e8bb5f81f5d53e890712cbdea0e99
Patrick Kennedy <mixelpix@mac.com>
2016-09-06 23:10:30 -0400

    -

--- nothing_is_something.rb
+++ nothing_is_something.rb
@@ -20,24 +20,29 @@ http://stackoverflow.com/questions/1821266/what-is-so-special-about-smalltalk
 "Sending messages to objects"
 =end
 
+
 # NOTE these are synonymous:
+puts "1.to_s and 1.send(:to_s) are synonymous:"
 puts 1.to_s         # <-- "1"
 puts 1.send(:to_s)  # <-- "1"
-
+puts
 puts "Does " + 1.to_s + " == " + 1.send(:to_s) + "?  " + (1.to_s == 1.send(:to_s)).to_s
-puts "Ayep... 1 == 1 is " + (1 == 1).to_s
-puts "Q: What is the true.class? (Hint: it's classy!!) A: " + true.class.to_s
+puts "Ayep... 1 == 1 is " + ((1 == 1) == (1.to_s == 1.send(:to_s))).to_s
 puts
 
 
 # NOTE these are also synonymous:
+puts "(1 + 1) and 1.send(:+, 1) are also synonymous:"
 puts "1 + 1 = " + (1 + 1).to_s                # <-- "1 + 1 = 2"
 puts "1.send(:+, 1) = " + 1.send(:+, 1).to_s  # <-- "1 + 1 = 2"
-
 # In the ".send(:+, 1)" instance you are sending the operator "+" to the
 # class "Fixnum" (value of '1') with the argument (or, 'operand') of "1"
-
+puts
 puts "Does " + (1 + 1).to_s + " == " + 1.send(:+, 1).to_s + "?  " + ((1 + 1) == (1.send(:+, 1))).to_s
+# NOTE http://stackoverflow.com/questions/212407/what-exactly-is-the-singleton-class-in-ruby
+puts
+puts "Q: What is the true.class? (Hint: it's classy!!) A: " + true.class.to_s
+puts "true is a \"singleton instance\" of TrueClass"
 puts
 
 
@@ -47,9 +52,11 @@ puts
 puts "1.class is a " + 1.class.to_s             # <-- Fixnum
 puts
 puts "Fixnum.instance_methods(false) are:" + Fixnum.instance_methods(false).to_s
+puts "A line by line, alphabetically sorted list:"
 puts Fixnum.instance_methods(false).sort
 puts
 puts "Fixnum.instance_methods(true) are: \n" + Fixnum.instance_methods(true).to_s
+puts "A much longer line by line, alphabetically sorted list:"
 puts Fixnum.instance_methods(true).sort
 puts
 
